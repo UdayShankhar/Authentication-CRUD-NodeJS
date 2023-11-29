de8a343929e7984023e3970e1286ea5c4548946b
Uday Shankhar <uday.shankhar@bloomsmobility.com>
2023-11-29 08:07:12 +0530

    Removed unused routes,schema and commented code

--- routes/auth.js
+++ routes/auth.js
@@ -1,68 +1,6 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-
-router.post("/register", async (req, res) => {
-  try {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: CryptoJS.AES.encrypt(
-        req.body.password,
-        "qewretrytufyiguoyt"
-      ).toString(),
-    });
-
-    const savedUser = await newUser.save();
-    const accessToken = jwt.sign(
-      {
-        id: savedUser._id,
-        isAdmin: savedUser.isAdmin,
-      },
-      "jbsakjdbiabsdibaskdbaksjdb",
-      {
-        expiresIn: "2d",
-      }
-    );
-
-    const { password, ...userDetails } = savedUser._doc;
-    res.status(200).json({ ...userDetails, accessToken });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// router.post("/login", async (req, res) => {
-//     try {
-//         const user = await User.findOne({
-//             username: req.body.username,
-//         })
-
-//         !user && res.status(401).json('Wrong credentials')
-
-//         const hashedPassword = CryptoJS.AES.decrypt(user.password, 'qewretrytufyiguoyt')
-//         const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8)
-
-//         Originalpassword != req.body.password && res.status(401).json('Wrong credentials')
-
-//         const accessToken = jwt.sign({
-//             id: user._id,
-//             isAdmin: user.isAdmin
-//         }, 'jbsakjdbiabsdibaskdbaksjdb', {
-//             expiresIn: '2d'
-//         })
-
-//         const { password, ...others } = user._doc
-
-//         res.status(200).json({ ...others, accessToken })
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send(error)
-//     }
-
-// })
 
 router.post("/login", async (req, res) => {
   try {
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,43 +1,21 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    console.log(req.headers)
-    const authHeader = req.headers.token || req.headers.authorization
-    if (authHeader) {
-        const token = authHeader.split(" ")[1]
-        console.log(token);
-        jwt.verify(token, 'jbsakjdbiabsdibaskdbaksjdb', (err, user) => {
-            if (err) res.status(403).json('Invalid Token')
-            req.user = user
-            next()
-        })
-    } else {
-        return res.status(401).json("You are not authenticated")
-    }
-}
-
-const verifyTokenAndAuthorization = (req, res, next) => {
-    verifyToken(req, res, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) {
-            next();
-        } else {
-            res.status(403).json("You are not alowed to do that!");
-        }
+  console.log(req.headers);
+  const authHeader = req.headers.token || req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, "jbsakjdbiabsdibaskdbaksjdb", (err, user) => {
+      if (err) res.status(403).json("Invalid Token");
+      req.user = user;
+      next();
     });
-};
-
-const verifyTokenAndAdmin = (req, res, next) => {
-    verifyToken(req, res, () => {
-        if (req.user.isAdmin) {
-            next();
-        } else {
-            res.status(403).json("You are not alowed to do that!");
-        }
-    });
+  } else {
+    return res.status(401).json("You are not authenticated");
+  }
 };
 
 module.exports = {
-    verifyToken,
-    verifyTokenAndAuthorization,
-    verifyTokenAndAdmin,
+  verifyToken,
 };
